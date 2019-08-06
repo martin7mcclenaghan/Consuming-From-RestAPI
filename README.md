f10ff13a02898c298e87f7236c0ad801781dd330
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-06 21:12:33 +0100

    Initial setup for repo. Need to add dependencies.

---
+++ README.md
@@ -0,0 +1,6 @@
+#Experimenting with data from public APIs!
+
+This is being done with Java 11 and Sring 2.1.6.
+
+I am aware that this is regularly used as an interview coding challenge so am keen
+to experiment with this.
---
+++ src/main/java/learning/example/ConsumingFromRest/ConsumingFromRestApplication.java
@@ -0,0 +1,13 @@
+package learning.example.ConsumingFromRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingFromRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingFromRestApplication.class, args);
+	}
+
+}
