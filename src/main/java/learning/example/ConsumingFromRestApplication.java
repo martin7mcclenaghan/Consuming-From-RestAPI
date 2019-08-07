e4db53bc8d1c530b5f8eaa4da608559acf0120b9
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-07 20:40:45 +0100

    Jackson added as dependency and moved Application class with @SpringBootApplication into learning.example.

--- src/main/java/learning/example/ConsumingFromRestApplication.java
+++ src/main/java/learning/example/ConsumingFromRestApplication.java
@@ -1,4 +1,4 @@
-package learning.example.ConsumingFromRest;
+package learning.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
