8ca681be3c49a63bd59a905df2ded7e5c1563665
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-09 15:43:46 +0100

    Added logic to PeopleData class that holds ArrayList created from data from public API. Added methods for addPerson (), removePerson (), getPerson() and updatePerson().

--- src/main/java/learning/example/PeopleData.java
+++ src/main/java/learning/example/PeopleData.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 @Component
 public class PeopleData {
@@ -25,6 +26,64 @@ public class PeopleData {
         this.dataArray = dataArray;
         log.info("Person List = {}", personList);
     }
+
+    // == public methods ==
+
+    public void addPerson (Person toAdd){
+
+        if(toAdd == null){
+            throw new NullPointerException("toAdd is a required parameter");
+        }
+
+        int currentLastId = personList.size();
+        toAdd.setId(currentLastId + 1);
+        personList.add(toAdd);
+
+    }
+
+    public void removePerson(int id){
+        ListIterator<Person> personListIterator = personList.listIterator();
+
+        while(personListIterator.hasNext()){
+
+            Person person = personListIterator.next();
+
+            if(person.getId() == id){
+                personListIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public Person getPerson (int id){
+
+        for(Person person : personList){
+            if(person.getId() == id){
+                return person;
+            }
+        }
+
+        return null;
+    }
+
+    public void updateItem(Person toUpdate){
+
+        if(toUpdate == null){
+            throw new NullPointerException("toUpdate is a required parameter");
+        }
+
+        ListIterator<Person> personListIterator = personList.listIterator();
+
+        while(personListIterator.hasNext()){
+            Person person = personListIterator.next();
+
+            if(person.equals(toUpdate)){
+                personListIterator.set(toUpdate);
+                break;
+            }
+        }
+    }
+
 
 
 }
