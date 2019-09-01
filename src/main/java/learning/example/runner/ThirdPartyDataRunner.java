1e01c4c58233067c079fc7e3030562bc1c18ec13
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-02 00:41:30 +0100

    ThirdPartyDataRunner now pulls in data from JSON placeholder. This will replace DataArray class and no longer relies on @PostContruct to be called.

--- src/main/java/learning/example/runner/ThirdPartyDataRunner.java
+++ src/main/java/learning/example/runner/ThirdPartyDataRunner.java
@@ -1,4 +1,40 @@
 package learning.example.runner;
 
-public class ThirdPartyDataRunner {
+import learning.example.Person;
+import learning.example.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+@Component
+public class ThirdPartyDataRunner implements CommandLineRunner {
+
+    // == constants ==
+    private Logger log = LoggerFactory.getLogger(ThirdPartyDataRunner.class);
+
+    //== fields ==
+    private final RestTemplate restTemplate;
+    private PersonRepository personRepository;
+
+    // == constructors ==
+    public ThirdPartyDataRunner(RestTemplate restTemplate, PersonRepository personRepository) {
+        this.restTemplate = restTemplate;
+        this.personRepository = personRepository;
+    }
+
+    // == implemented methods ==
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Getting data from jsonplaceholder");
+        Person[] peopleArray = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users", Person[].class);
+
+        if (peopleArray == null) {
+            throw new RuntimeException("Unable to get data");
+        }
+        personRepository.saveAll(Arrays.asList(peopleArray));
+    }
 }
