37c005774ea268f7ed93edd5de474d1a7ec753ff
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-03 00:04:22 +0100

    Cleaned up comments.

--- src/main/java/learning/example/controller/PersonController.java
+++ src/main/java/learning/example/controller/PersonController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class PersonController {
 
-    // == constants //
+    // == constants ==
     private static final Logger log = LoggerFactory.getLogger(PersonController.class);
 
     // == fields ==
